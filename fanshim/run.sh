af44416cd577e19966d16ed845e989472b15ab36
Joao-Capelo <95879618+Joao-Capelo@users.noreply.github.com>
2026-04-14 19:04:32 +0100

    Update run.sh

--- fanshim/run.sh
+++ fanshim/run.sh
@@ -3,7 +3,7 @@
 ON_TEMP=$(bashio::config 'on_temperature')
 OFF_TEMP=$(bashio::config 'off_temperature')
 
-echo "Starting FanShim control"
-echo "ON: $ON_TEMP °C | OFF: $OFF_TEMP °C"
+echo "FanShim Controller starting"
+echo "ON: $ON_TEMP | OFF: $OFF_TEMP"
 
 python3 /fan_control.py "$ON_TEMP" "$OFF_TEMP"
