cd6949a780393adaa2188f34cb00aa2873a953e4
Joao-Capelo <95879618+Joao-Capelo@users.noreply.github.com>
2026-04-14 19:10:31 +0100

    Delete fanshim directory

--- fanshim/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/with-contenv bashio
-
-ON_TEMP=$(bashio::config 'on_temperature')
-OFF_TEMP=$(bashio::config 'off_temperature')
-
-echo "FanShim Controller starting"
-echo "ON: $ON_TEMP | OFF: $OFF_TEMP"
-
-python3 /fan_control.py "$ON_TEMP" "$OFF_TEMP"
